42d85f60ce7f4146b24684a3e4696cfb0a398f02
Simon Swan <shk.swan@gmail.com>
2023-09-15 00:13:09 +0000

    Add tabset and update dependencies (#16)
    * Use asciidoctor/tabs and update other packages

---
+++ asciinema-macro/lib/asciinema-macro.js
@@ -0,0 +1,42 @@
+
+const createDiv = function (target, attrs) {
+  const theme = attrs.theme ? attrs.theme : 'solarized-light'
+  const autoplay = attrs.autoplay ? attrs.autoplay : 'true'
+  const loop = attrs.loop ? attrs.loop : 'true'
+  const asciiDiv = `<div id="${target}" class="openblock"></div>`
+  const props = `{loop: ${loop}, theme: '${theme}', autoplay: ${autoplay}, controls: false}`
+  const asciiScript = `AsciinemaPlayer.create('./_images/${target}.cast', document.getElementById('${target}'), ${props});`
+  return `${asciiDiv}<script>${asciiScript}</script>`
+}
+
+const asciinemaMacro = function () {
+  const self = this
+
+  self.named('asciinema')
+  self.positionalAttributes(['theme', 'autoplay', 'loop'])
+
+  self.process(function (parent, target, attrs) {
+    const html = createDiv(target, attrs)
+    return self.createBlock(parent, 'pass', html, attrs, {})
+  })
+}
+
+module.exports.register = function (registry) {
+  function doRegister (registry) {
+    if (typeof registry.blockMacro === 'function') {
+      registry.blockMacro(asciinemaMacro)
+    } else {
+      console.warn('no \'blockMacro\' method on alleged registry')
+    }
+  }
+
+  if (typeof registry.register === 'function') {
+    registry.register(function () {
+      registry = this
+      doRegister(registry)
+    })
+  } else {
+    doRegister(registry)
+  }
+  return registry
+}
--- gulp.d/tasks/build-preview-pages.js
+++ gulp.d/tasks/build-preview-pages.js
@@ -6,7 +6,7 @@ Error.call = (self, ...args) => {
   return Object.assign(self, { message: err.message, stack: err.stack })
 }
 
-const asciidoctor = require('asciidoctor.js')()
+const Asciidoctor = require('@asciidoctor/core')()
 const fs = require('fs-extra')
 const handlebars = require('handlebars')
 const merge = require('merge-stream')
@@ -27,7 +27,21 @@ module.exports = (src, previewSrc, previewDest, sink = () => map()) => (done) =>
       merge(compileLayouts(src), registerPartials(src), registerHelpers(src), copyImages(previewSrc, previewDest))
     ),
   ])
-    .then(([baseUiModel, { layouts }]) => [{ ...baseUiModel, env: process.env }, layouts])
+    .then(([baseUiModel, { layouts }]) => {
+      const extensions = ((baseUiModel.asciidoc || {}).extensions || []).map((request) => {
+        ASCIIDOC_ATTRIBUTES[request.replace(/^@|\.js$/, '').replace(/[/]/g, '-') + '-loaded'] = ''
+        const extension = require(request)
+        extension.register(Asciidoctor.Extensions)
+        return extension
+      })
+      const asciidoc = { extensions }
+      for (const component of baseUiModel.site.components) {
+        for (const version of component.versions || []) version.asciidoc = asciidoc
+      }
+      baseUiModel = { ...baseUiModel, env: process.env }
+      delete baseUiModel.asciidoc
+      return [baseUiModel, layouts]
+    })
     .then(([baseUiModel, layouts]) =>
       vfs
         .src('**/*.adoc', { base: previewSrc, cwd: previewSrc })
@@ -42,7 +56,7 @@ module.exports = (src, previewSrc, previewDest, sink = () => map()) => (done) =>
             if (file.stem === '404') {
               uiModel.page = { layout: '404', title: 'Page Not Found' }
             } else {
-              const doc = asciidoctor.load(file.contents, { safe: 'safe', attributes: ASCIIDOC_ATTRIBUTES })
+              const doc = Asciidoctor.load(file.contents, { safe: 'safe', attributes: ASCIIDOC_ATTRIBUTES })
               uiModel.page.attributes = Object.entries(doc.getAttributes())
                 .filter(([name, val]) => name.startsWith('page-'))
                 .reduce((accum, [name, val]) => {
@@ -110,7 +124,7 @@ function compileLayouts (src) {
 
 function copyImages (src, dest) {
   return vfs
-    .src('**/*.{png,svg}', { base: src, cwd: src })
+    .src('**/*.{png,svg,cast}', { base: src, cwd: src })
     .pipe(vfs.dest(dest))
     .pipe(map((file, enc, next) => next()))
 }
